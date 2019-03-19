cf419333e5323560a5a5eb61dc8d3d5eb261bdd6
TANUJ DRALL <Tanuj9043>
2019-03-20 00:19:06 +0530

    Now

--- app.py
+++ app.py
@@ -88,8 +88,8 @@ api.add_resource(emotionAPI, '/api/emotionImage')
 api.add_resource(frontAPI, '/api/frontImage')
 api.add_resource(rearAPI, '/api/rearImage')
 
-# app.run(host=os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)))
+app.run(host=os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)))
 
 if __name__ == '__main__':
-     # app.run(debug=False)
-	app.run(port=5000,debug=False)
+     app.run(debug=False)
+	# app.run(port=5000,debug=False)
