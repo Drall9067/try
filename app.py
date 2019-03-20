4e66ccae50849e45983ef92975171de23afb3a2c
TANUJ DRALL <Tanuj9043>
2019-03-21 00:50:09 +0530

    Latest

--- app.py
+++ app.py
@@ -143,4 +143,4 @@ app.run(host=os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)))
 
 if __name__ == '__main__':
      app.run(debug=False)
-	app.run(port=5000,debug=False)
+	# app.run(port=5000,debug=False)
